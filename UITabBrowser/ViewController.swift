6968e2f70ba1f71570e57f342cedf32a532fbef2
ogaoga <tsutomu.ogasawara+github@gmail.com>
2021-02-27 16:07:47 +0900

    Add initial project

---
+++ UITabBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITabBrowser
+//
+//  Created by ogaoga on 2021/02/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
